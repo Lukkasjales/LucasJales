b175b0232bee45cd3a9557bbbba0e908d3b2fb3c
unknown <professor@ifpbcrlab.intra>
2023-01-13 09:10:33 -0300

    adição de operação de inserção de dados no db

--- index.js
+++ index.js
@@ -2,6 +2,52 @@ const express = require('express');
 const server = express();
 const path = require('path');
 
+(async() => {
+ const database = require('./db');
+ const Perfil = require('./modelos/perfil');
+ 
+ try{
+    const resultado = await database.sync();
+    console.log(resultado);
+
+    
+    const resultadoCreate = await Perfil.create({
+        nome: 'luciano 6',
+        sobrenome: 'alexandre',
+        telefone: '99999999',
+        bairro: 'bairro teste',
+        rua: 'rua teste',
+        cep: '5900000',
+        estado: 'PB',
+        email: 'luciano.silva@ifpb.edu.br',
+        pais: 'BRASIL',
+        login: 'luciano',
+        senha: 'senha123'
+    })
+
+    const resultadoCreate2 = await Perfil.create({
+        nome: 'luciano 5',
+        sobrenome: 'alexandre',
+        telefone: '99999999',
+        bairro: 'bairro teste',
+        rua: 'rua teste',
+        cep: '5900000',
+        estado: 'PB',
+        email: 'luciano.silva@ifpb.edu.br',
+        pais: 'BRASIL',
+        login: 'luciano',
+        senha: 'senha123'
+    })
+
+    console.log(resultadoCreate);
+    console.log(resultadoCreate2);
+
+ }catch(error){
+    console.log(error);
+ }
+})();
+
+
 server.get('/perfil', (req, res)=>{
     res.sendFile(path.join(__dirname+'/pages/perfil.html'));   
 })
